d72afdbadf964d05d4c5ad57a7d0a05dff313457
jmh26 <jorgemartinherrera2004@gmail.com>
2023-09-18 20:24:20 +0200

    Lunes 18

---
+++ app/src/main/java/com/example/ejerciciot1/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.ejerciciot1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val fondoBoton = findViewById<Button>(R.id.)
+        val boton = findViewById<Button>(R.id.button)
+
+        boton.setOnClickListener{
+            boton.text="Enviado"
+            boton.
+        }
+    }
+}
