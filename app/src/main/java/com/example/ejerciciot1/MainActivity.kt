0e7d0cdb2fbe15a5b17b5647d4a1df11c6811579
jmh26 <jorgemartinherrera2004@gmail.com>
2023-09-22 19:01:41 +0200

    Viernes 22

--- app/src/main/java/com/example/ejerciciot1/MainActivity.kt
+++ app/src/main/java/com/example/ejerciciot1/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        //val fondoBoton = findViewById<Button>(R.id.)
+
         val boton = findViewById<Button>(R.id.button)
 
         boton.setOnClickListener{
